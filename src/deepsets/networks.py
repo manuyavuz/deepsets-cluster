32268972e0ec22f71979647234bd88a3426ce944
Muhammed Yavuz Nuzumlali <manuyavuz@gmail.com>
2021-08-01 17:48:13 -0400

    Refactor network structure

--- src/deepsets/networks.py
+++ src/deepsets/networks.py
@@ -55,30 +55,23 @@ class InvariantModel(nn.Module):
         return namedtuple('InvariantModel', ['out', 'w_out', 'w_orig'])(out, w_out.reshape(-1, w_out.size(2)), w_orig.reshape(-1, w_orig.size(2)))
 
 class SmallMNISTCNNPhi(nn.Module):
-    def __init__(self, softmax=False):
+    def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
+        self.conv2_drop = nn.Dropout2d(p=0.2)
         self.fc1 = nn.Linear(320, 50)
-        self.fc1_drop = nn.Dropout2d()
+        self.fc1_drop = nn.Dropout2d(p=0.2)
         self.fc2 = nn.Linear(50, 10)
-        self.softmax = softmax
 
     def forward(self, x: NetIO) -> NetIO:
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = self.conv2_drop(self.conv2(x))
-        x = F.relu(F.max_pool2d(x, 2))
+        x = F.elu(F.max_pool2d(self.conv1(x), 2))
+        x = self.conv2(x)
+        x = self.conv2_drop(x)
+        x = F.elu(F.max_pool2d(x, 2))
         x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = self.fc1_drop(x)
-        if self.softmax:
-            x = F.softmax(self.fc2(x))
-        else:
-            x = F.relu(self.fc2(x))
+        x = self.fc2(self.fc1_drop(F.elu(self.fc1(x))))
         return x
-
-
 class SmallRho(nn.Module):
     def __init__(self, input_size: int, output_size: int = 1):
         super().__init__()
@@ -86,11 +79,11 @@ class SmallRho(nn.Module):
         self.output_size = output_size
 
         self.fc1 = nn.Linear(self.input_size, 10)
+        self.dropout1 = nn.Dropout(p=0.2)
         self.fc2 = nn.Linear(10, self.output_size)
 
     def forward(self, x: NetIO) -> NetIO:
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = self.fc2(self.dropout1(F.elu(self.fc1(x))))
         return x
 
 
@@ -101,8 +94,7 @@ class ClusterClf(nn.Module):
         self.output_size = output_size
         self.fc1 = nn.Linear(self.input_size, self.output_size)
         self.net = nn.Sequential(
-            self.fc1,
-            nn.Dropout()
+            self.fc1
         )
 
     def forward(self, x: NetIO, y=None) -> NetIO:
